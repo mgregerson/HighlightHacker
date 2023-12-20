1b357e78c8d02ce7e25560e3830f59daf12d6f78
Matt Gregerson <mgregerson4321@gmail.com>
2023-12-20 13:33:42 -0500

    added new userItem and recommended components, updated layout with suspensse while fetching users

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,15 +1,29 @@
+import { getRecommended } from "@/lib/recommended-service";
+import { Recommended, RecommendedSkeleton } from "./recommended";
 import Toggle from "./toggle";
 import Wrapper from "./wrapper";
 
-function Sidebar() {
+export async function Sidebar() {
   // Can fetch followed users
+  const recommended = await getRecommended();
   // Fetch recommended users
 
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommended data={recommended} />
+      </div>
     </Wrapper>
   );
 }
+
+export const SidebarSkeleton = () => {
+  return (
+    <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+      <RecommendedSkeleton />
+    </aside>
+  );
+};
 
 export default Sidebar;
--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,16 +1,18 @@
+import { Suspense } from "react";
 import Container from "./_components/container";
 import Navbar from "./_components/navbar";
-import Sidebar from "./_components/sidebar";
+import Sidebar, { SidebarSkeleton } from "./_components/sidebar";
 
 function BrowseLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       <div className="flex h-full pt-20">
+        <Suspense fallback={<SidebarSkeleton/>}>
         <Sidebar />
-        <Container>
-        {children}
-        </Container>
+        </Suspense>
+
+        <Container>{children}</Container>
       </div>
     </>
   );
