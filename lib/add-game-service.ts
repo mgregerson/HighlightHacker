f66e42d7490049adc5cb46e5563097a63154734d
Matt Gregerson <mgregerson4321@gmail.com>
2023-12-22 17:29:25 -0500

    New service that allows us to create a group of highlights at once.

---
+++ lib/add-game-service.ts
@@ -0,0 +1,23 @@
+import { db } from "@/lib/db";
+
+export async function importSportsData(games: any[]) {
+  try {
+    const addedGames = [];
+      for (let game of games){
+        const createdGame = await db.highlight.create({
+            data: {
+              url: game.url,
+              description: game.description,
+              youtubeId: game.youtubeId,
+              sport: game.sport
+            },
+          });
+          // Log success message
+          console.log(`Successfully created a new game with ID: ${createdGame.id}`);
+          addedGames.push(createdGame);
+    }
+    return addedGames;
+  } catch (error) {
+    console.error("Error importing data:", error);
+  }
+}
