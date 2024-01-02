6349a16f031226f12010395e530d563ce66de560
Matt Gregerson <mgregerson4321@gmail.com>
2024-01-02 16:52:10 -0500

    Added 10 most recent highlights to home page of website

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,10 +1,16 @@
-import { UserButton } from "@clerk/nextjs"
+import { getRecentHighlights } from "@/lib/highlight-service"
+import Highlights from "../[sport]/_components/highlights";
 
+export default async function Home() {
 
-export default function Home() {
+  const recentHighlights = await getRecentHighlights();
+  
+  console.log('recentHighlights=', recentHighlights)
+
   return (
    <div className="flex flex-col gap-y-4">
-    <h1>Home page</h1>
+    <h1 className="pt-5 text-5xl text-center">Our Most Recent Highlights</h1>
+    {recentHighlights && <Highlights highlights={recentHighlights}/>}
    </div>
   )
 }
