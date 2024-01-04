421a3dc005816ff13c964f5a134281925bc07ada
Matt Gregerson <mgregerson4321@gmail.com>
2024-01-04 12:39:53 -0500

    destructured highlight component

--- components/Highlight.tsx
+++ components/Highlight.tsx
@@ -1,37 +1,22 @@
-"use client";
 import { Highlight } from "@prisma/client";
-import YouTube, { YouTubeProps } from "react-youtube";
+import { isLikingHighlight } from "@/lib/like-service";
+import LikeActions from "@/app/(browse)/[sport]/_components/likeActions";
+import HighlightVideo from "./HighlightVideo";
 
 interface HighlightProps {
   highlight: Highlight;
 }
 
-function HighlightPage({ highlight }: HighlightProps) {
-  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  };
+async function HighlightPage({ highlight }: HighlightProps) {
+  const isLiking = await isLikingHighlight(highlight.id);
 
-  const opts: YouTubeProps["opts"] = {
-    height: "390",
-    width: "640",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
-  };
+  console.log("isLiking=", isLiking);
 
   return (
     <div className="max-w-2xl mx-auto mt-8 p-4 bg-white shadow-md rounded-md">
       {/* <h1 className="text-2xl font-bold mb-4 text-blue-700">{highlight.title}</h1> */}
-      <div className="flex justify-center">
-        <YouTube
-          videoId={highlight.youtubeId}
-          opts={opts}
-          onReady={onPlayerReady}
-        />
-      </div>
-      <p className="text-gray-600 mt-4">{highlight.description}</p>
+      <HighlightVideo highlight={highlight} />
+      <LikeActions isLiking={isLiking} highlightId={highlight.id} />
     </div>
   );
 }
