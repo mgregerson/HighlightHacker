b06e73fcc78d7356d6c8cf189a95ef17b2d53468
Matt Gregerson <mgregerson4321@gmail.com>
2024-01-04 12:36:13 -0500

    Updated to utilize like-service

--- lib/highlight-service.ts
+++ lib/highlight-service.ts
@@ -17,18 +17,30 @@ export const getHighlights = async (sport: string) => {
   return highlights;
 };
 
-export const getRecentHighlights = async () => {
+export const getRecentHighlights = async (cursor: string | null = null) => {
   let highlights; 
 
   try {
-    highlights = await db.highlight.findMany({
-      take: 10,
-      orderBy: {
-        createdAt: 'desc',
-      },
-    })
-
-    return highlights;
+    if (cursor !== null) {
+      highlights = await db.highlight.findMany({
+        take: 10,
+        cursor: {
+          id: cursor
+        },
+        orderBy: {
+          createdAt: 'desc'
+        }
+      })
+      return highlights;
+    } else {
+      highlights = await db.highlight.findMany({
+        take: 10,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      })
+      return highlights;
+    }
   } catch (err) {
     console.error('Error fetching recent highlights:', err);
     throw err;
---
+++ lib/like-service.ts
@@ -0,0 +1,111 @@
+import { db } from "./db";
+
+import { getSelf } from "@/lib/auth-service";
+
+
+export const isLikingHighlight = async (highlightId: string) => {
+    try {
+      const self = await getSelf();
+
+      const highlight = await db.highlight.findUnique({
+        where: { id: highlightId },
+        include: { likes: true },
+    });
+  
+      if (!highlight) {
+        throw new Error("Highlight Not Found");
+      }
+  
+      // Check if the user is following the sport
+      const likedHighlight = await db.like.findFirst({
+        where: {
+            userId: self?.id,
+            highlightId: highlight.id,
+        }
+      })
+
+      if (likedHighlight) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch {
+      return false;
+    }
+  };
+  
+
+export const likeHighlight = async (highlightId: string) => {
+    const self = await getSelf();
+
+    const highlight = await db.highlight.findUnique({
+        where: { id: highlightId },
+        include: { likes: true },
+    });
+
+    if (!highlight) {
+        throw new Error("Highlight Not Found");
+    }
+
+    // Check if the user has liked the highlight
+    const existingLike = await db.like.findFirst({
+        where: {
+            userId: self?.id,
+            highlightId: highlight.id,
+        }
+    });
+
+    if (existingLike){
+        throw new Error("Already Liked Highlight");
+    }
+
+    const like = await db.like.create({
+        data: {
+            userId: self!.id,
+            highlightId: highlight.id,
+        },
+        include: {
+            user: true,
+            highlight: true,
+        }
+    })
+
+    return like;
+}
+
+export const unlikeHighlight = async (highlightId: string) => {
+    const self = await getSelf();
+
+    const highlight = await db.highlight.findUnique({
+        where: { id: highlightId },
+        include: { likes: true },
+    });
+
+    if (!highlight) {
+        throw new Error("Highlight Not Found");
+    }
+
+    // Check if the user has liked the highlight
+    const existingLike = await db.like.findFirst({
+        where: {
+            userId: self?.id,
+            highlightId: highlight.id,
+        }
+    });
+
+    if (!existingLike){
+        throw new Error(`${self?.username} hasn't Liked This Highlight`);
+    }
+
+    const like = await db.like.delete({
+        where: {
+            id: existingLike.id
+        },
+        include: {
+            user: true,
+            highlight: true,
+        }
+    })
+
+    return like;
+}
