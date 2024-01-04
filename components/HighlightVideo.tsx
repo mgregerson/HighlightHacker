13a2bb41ea22afdbe6773741bf1701e9b52e74cd
Matt Gregerson <mgregerson4321@gmail.com>
2024-01-04 12:36:30 -0500

    Added new file that renders an individual video

---
+++ components/HighlightVideo.tsx
@@ -0,0 +1,40 @@
+'use client';
+import { Highlight } from "@prisma/client";
+import YouTube, { YouTubeProps } from "react-youtube";
+
+interface HighlightProps {
+  highlight: Highlight;
+}
+
+async function HighlightVideo({ highlight }: HighlightProps) {
+  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+
+  const opts: YouTubeProps["opts"] = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="max-w-2xl mx-auto mt-8 p-4 bg-white shadow-md rounded-md">
+      {/* <h1 className="text-2xl font-bold mb-4 text-blue-700">{highlight.title}</h1> */}
+      <div className="flex justify-center">
+        <YouTube
+          videoId={highlight.youtubeId}
+          opts={opts}
+          onReady={onPlayerReady}
+        />
+      </div>
+      <p className="text-gray-600 mt-4">{highlight.description}</p>
+    </div>
+  );
+}
+
+export default HighlightVideo;
