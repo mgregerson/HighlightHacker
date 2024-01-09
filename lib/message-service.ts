a829d21d0dc610912839cf32bbe27038ede77805
Matt Gregerson <mgregerson4321@gmail.com>
2024-01-08 20:23:34 -0500

    Updated message-service to be formatted correctly

--- lib/message-service.ts
+++ lib/message-service.ts
@@ -2,34 +2,45 @@ import { db } from "./db";
 
 import { getSelf } from "@/lib/auth-service";
 
-export const sendMessage = async (message: string, chatroomId: string,) => {
-    const self = await getSelf();
-    try {
-        const newMessage = await db.message.create({
-            data: {
-                content: message,
-                chatroomId,
-                userId: self!.id,
+export const sendMessage = async (content: string, chatroomId: string) => {
+  const self = await getSelf();
+  try {
+    const newMessage = await db.message.create({
+      data: {
+        content,
+        chatroomId,
+        userId: self!.id,
+      },
+      include: {
+        chatroom: {
+          include: {
+            highlight: {
+              include: {
+                sport: true,
+              },
             },
-        });
-        return newMessage;
-    } catch (err) {
-        throw new Error("Internal Error");
-    }
-}
+          },
+        },
+      },
+    });
+    return newMessage;
+  } catch (err) {
+    throw new Error("Internal Error");
+  }
+};
 
 export const getMessagesByChatroom = async (chatroomId: string) => {
-    try {
-        const messages = await db.message.findMany({
-            where: {
-                chatroomId,
-            },
-            include: {
-                user: true,
-            },
-        });
-        return messages;
-    } catch (err) {
-        throw new Error("Internal Error");
-    }
-}
+  try {
+    const messages = await db.message.findMany({
+      where: {
+        chatroomId,
+      },
+      include: {
+        user: true,
+      },
+    });
+    return messages;
+  } catch (err) {
+    throw new Error("Internal Error");
+  }
+};
