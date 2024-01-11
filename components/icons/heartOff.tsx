b73681d5a92ed97dd8a38c88099d92f580403a73
Matt Gregerson <mgregerson4321@gmail.com>
2024-01-11 16:17:58 -0500

    Added heart icons to componentsfor liking messages

---
+++ components/icons/heartOff.tsx
@@ -0,0 +1,9 @@
+import { HeartOff } from 'tabler-icons-react';
+              
+export default function DislikedHeart() {
+  return <HeartOff
+    size={40}
+    strokeWidth={1.5}
+    color={'#bf4040'}
+  />;
+}
---
+++ components/icons/heartOn.tsx
@@ -0,0 +1,9 @@
+import { Heart } from 'tabler-icons-react';
+              
+export default function LikedHeart() {
+  return <Heart
+    size={40}
+    strokeWidth={1.5}
+    color={'#bf4040'}
+  />;
+}
